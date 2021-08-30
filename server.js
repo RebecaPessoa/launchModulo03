30d2f3ff7670fcffac0763a4865e621586703c63
RebecaPessoa <rebecahayanny17@gmail.com>
2021-08-30 02:27:31 -0300

    ajuste de blocos nunjuck

--- server.js
+++ server.js
@@ -17,8 +17,14 @@ nunjucks.configure("views", {
 
 
 server.get("/", function(req,res){
-    return res.render("index")
+    return res.render("about")
 })
+
+server.get("/portfolio", function(req,res){
+    return res.render("portfolio")
+})
+
+
 server.listen(5000, function(){
     console.log("server is running")
 })
